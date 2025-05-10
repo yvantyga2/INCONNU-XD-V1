4484179fb1a8b57a43d288dc9b63bc736db5d2c0
INCONNU BOY <inconnuboy39@gmail.com>
2025-05-10 14:36:46 -0300

    Update and rename main-menu.js to old.js

--- plugins/old.js
+++ plugins/old.js
@@ -64,7 +64,7 @@ const menu = async (m, Matrix) => {
   const mode = config.MODE === 'public' ? 'public' : 'private';
   const pref = config.PREFIX;
 
-  const validCommands = ['fullmenu', 'menu2', 'listcmd'];
+  const validCommands = ['inconnu', 'menu', 'bot'];
 
   if (validCommands.includes(cmd)) {
     const str = `
